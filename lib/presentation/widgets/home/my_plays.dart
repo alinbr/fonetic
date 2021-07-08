26a5fbd9335eb05ef621f79f92c2ecd4f5a8fe75
Alin-Gabriel Musat <musataling@gmail.com>
2021-07-08 23:50:02 +0300

    Add view all card

--- lib/presentation/widgets/home/my_plays.dart
+++ lib/presentation/widgets/home/my_plays.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:fonetic/infrastructure/dtos/play.dart';
+import 'package:fonetic/presentation/screens/my_plays_screen.dart';
 import 'package:fonetic/presentation/widgets/home/my_play_card.dart';
 
 class MyPlays extends StatelessWidget {
@@ -23,19 +24,65 @@ class MyPlays extends StatelessWidget {
               .copyWith(fontWeight: FontWeight.w700),
         ),
       ),
-      Container(
-        height: 250.h,
-        width: double.infinity,
-        child: ListView.builder(
-            itemCount: plays.length,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (ctx, index) {
-              return MyPlayCard(
-                play: plays[index],
-                first: index == 0,
-              );
-            }),
-      )
+      plays.length > 0
+          ? Container(
+              height: 250.h,
+              width: double.infinity,
+              child: ListView.builder(
+                  itemCount: plays.length + 1,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (ctx, index) {
+                    if (index == plays.length) {
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute<void>(
+                            builder: (BuildContext context) {
+                              return MyPlaysScreen();
+                            },
+                          ));
+                        },
+                        child: Column(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(8),
+                                color: Theme.of(context).backgroundColor,
+                              ),
+                              height: 168.h,
+                              width: 168.h,
+                              child: Center(
+                                child: Text(
+                                  "View all",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .headline5!
+                                      .copyWith(fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                    return MyPlayCard(
+                      play: plays[index],
+                      first: index == 0,
+                    );
+                  }))
+          : Container(
+              height: 100.h,
+              child: Opacity(
+                opacity: 0.25,
+                child: Center(
+                    child: Text(
+                  "No plays yet 😢",
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline5!
+                      .copyWith(fontWeight: FontWeight.bold),
+                )),
+              ),
+            )
     ]);
   }
 }
